9b976cf5c2653c4d703eb7e38061a74683da64fc
Administrator <admin@example.com>
2017-12-25 02:24:27 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,6 +6,10 @@
 export const environment = {
   production: false,
 
-  SERVER_URL: 'http://localhost:3001/',
+  //SERVER_URL: 'http://localhost:3001/',
+  //IMAGE_BASE_URL: 'https://speegar.com/images/'
+  
+  
+  SERVER_URL: 'https://speegar.com:3005/',
   IMAGE_BASE_URL: 'https://speegar.com/images/'
 };
