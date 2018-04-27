eba2540a84ab26d8467bbaa5730461aca9faf001
zied <ziedsaidig@gmail.com>
2018-04-27 19:22:38 +0100

    test updatePictureUser

--- src/app/main/profile/profile.ts
+++ src/app/main/profile/profile.ts
@@ -482,7 +482,7 @@ export class Profile {
         if (response.status == "0") {
 
 
-           console.log(response)
+
 
           if (this.loginService.isWasConnectedWithFacebook){
             let fuser = this.loginService.getFacebookUser();
@@ -496,7 +496,8 @@ export class Profile {
 
           this.uploadedProfilePicture = null;
           jQuery("#file-profile").val("");
-          jQuery(".profile-photo").css('background-image', 'url(' + ')');
+          this.profilePictLoad = false;
+          jQuery(".profile-photo").css('background-image', 'url(' +response.profile.profilePicture || ""+')');
           this.profilePictLoad = false;
           swal({
             title: this.translateCode("profile_update_picture_popup_notification_title"),
