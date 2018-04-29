a8a065bf75ae8d97a8cfca09a55ad2eb24d73e4c
Administrator <admin@example.com>
2018-04-30 01:02:40 +0200

    Update suggestions.component.ts

--- src/app/main/suggestions/suggestions.component.ts
+++ src/app/main/suggestions/suggestions.component.ts
@@ -43,7 +43,7 @@ export class SuggestionsComponent implements OnInit {
       .map((res: Response) => res.json())
       .subscribe(
         response => {
-          Array.prototype.push.apply(this.popularProfiles, response.profiles);
+        if(response.profiles.length>0) {this.popularProfiles.push(...response.profiles)} 
         },
         err => {
         },
