a55c2fd4231a13a876d4d9f0563d6a65d97e3d3a
samet-ma <mohamedamin.samet@yandex.com>
2018-05-01 23:22:23 +0100

    top blagueurs

--- src/app/topBlagueursAndDecov/topBlagueursAndDecov.ts
+++ src/app/topBlagueursAndDecov/topBlagueursAndDecov.ts
@@ -1,18 +1,18 @@
-import {Component, ChangeDetectorRef, Output, EventEmitter} from '@angular/core';
-import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import { Component, ChangeDetectorRef, Output, EventEmitter } from '@angular/core';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { Router } from '@angular/router';
 import 'rxjs/add/operator/map';
 
 /* conf */
-import {AppSettings} from '../conf/app-settings';
-import {environment} from "../../environments/environment";
+import { AppSettings } from '../conf/app-settings';
+import { environment } from "../../environments/environment";
 
 /* services */
-import {LoginService} from '../service/loginService';
+import { LoginService } from '../service/loginService';
 import { TranslateService } from 'ng2-translate';
 
 /* beans  */
-import {User} from '../beans/user';
+import { User } from '../beans/user';
 
 /** Utils */
 import * as pathUtils from '../utils/path.utils';
@@ -25,65 +25,68 @@ import * as pathUtils from '../utils/path.utils';
 })
 export class TopBlagueursAndDecov {
 
-  public popularProfiles:Array <User> = [];
+  public popularProfiles: Array<User> = [];
   displayedNumberPopularProfiles = 4;
+  displayShowMore: boolean = true;
   //changes
   lastPopularProfileID;
   //
-  public user:User = new User();
+  public user: User = new User();
 
   @Output() loadPublications = new EventEmitter<any>();
 
-  constructor(public translate:TranslateService,
-              private http:Http,
-              private router:Router,
-              private loginService:LoginService,
-              private changeDetector:ChangeDetectorRef) {
+  constructor(public translate: TranslateService,
+    private http: Http,
+    private router: Router,
+    private loginService: LoginService,
+    private changeDetector: ChangeDetectorRef) {
     loginService.redirect();
     this.user = loginService.user;
     this.loadPopularProfiles();
   }
 
-  loadMoreProfiles(){
-    this.displayedNumberPopularProfiles=this.displayedNumberPopularProfiles+4;
+  loadMoreProfiles() {
+    this.displayedNumberPopularProfiles = this.displayedNumberPopularProfiles + 4;
+    this.displayShowMore = this.displayedNumberPopularProfiles < 12 &&
+      										 this.popularProfiles.length >= this.displayedNumberPopularProfiles;
   }
 
-  loadPopularProfiles(Id_Profile?:string) {
-	var url: string = environment.SERVER_URL + pathUtils.GET_POPULAR_PROFILES + '/';
-	if (Id_Profile) { url += Id_Profile }
-	this.http.get(url,
-	  AppSettings.OPTIONS)
-	  .map((res: Response) => res.json())
-	  .subscribe(
-	    response => {
-	      Array.prototype.push.apply(this.popularProfiles, response.profiles);
-	      //changes
-	      if (response.profiles && response.profiles.length) {
-	        this.lastPopularProfileID = response.profiles[response.profiles.length - 1]._id;
-	      }
-	      //
-	    },
-	    err => {
-	    },
-	    () => {
-	      this.changeDetector.markForCheck();
-	    }
-	  );
+  loadPopularProfiles(Id_Profile?: string) {
+    var url: string = environment.SERVER_URL + pathUtils.GET_POPULAR_PROFILES + '/';
+    if (Id_Profile) { url += Id_Profile }
+    this.http.get(url,
+      AppSettings.OPTIONS)
+      .map((res: Response) => res.json())
+      .subscribe(
+        response => {
+          Array.prototype.push.apply(this.popularProfiles, response.profiles);
+          //changes
+          if (response.profiles && response.profiles.length) {
+            this.lastPopularProfileID = response.profiles[response.profiles.length - 1]._id;
+          }
+          //
+        },
+        err => {
+        },
+        () => {
+          this.changeDetector.markForCheck();
+        }
+      );
   }
 
-//changes
-  loadMore(){
-if (this.popularProfiles.length<6){
-  this.loadPopularProfiles(this.lastPopularProfileID);
-}
+  //changes
+  loadMore() {
+    if (this.popularProfiles.length < 6) {
+      this.loadPopularProfiles(this.lastPopularProfileID);
+    }
 
   }
-//
+  //
 
 
 
 
-  subscribe(user:User) {
+  subscribe(user: User) {
     let body = JSON.stringify({
       profileId: user._id
     });
@@ -93,24 +96,24 @@ if (this.popularProfiles.length<6){
       body,
       AppSettings.OPTIONS
     )
-      .map((res:Response) => res.json())
+      .map((res: Response) => res.json())
       .subscribe(
         response => {
-        if (response.status == 0) {
-          this.popularProfiles.splice(this.popularProfiles.indexOf(user), 1);
-          this.loadMore();
-        }
-      },
+          if (response.status == 0) {
+            this.popularProfiles.splice(this.popularProfiles.indexOf(user), 1);
+            this.loadMore();
+          }
+        },
         err => {
-      },
-      () => {
-        this.changeDetector.markForCheck();
-      }
-    );
-    
+        },
+        () => {
+          this.changeDetector.markForCheck();
+        }
+      );
+
   }
 
-  ignore(user:User) {
+  ignore(user: User) {
     let body = JSON.stringify({
       profileId: user._id
     });
@@ -120,25 +123,25 @@ if (this.popularProfiles.length<6){
       body,
       AppSettings.OPTIONS
     )
-      .map((res:Response) => res.json())
+      .map((res: Response) => res.json())
       .subscribe(
         response => {
-        if (response.status == 0) {
-          this.popularProfiles.splice(this.popularProfiles.indexOf(user), 1);
-          this.loadMore();
-        }
-      },
+          if (response.status == 0) {
+            this.popularProfiles.splice(this.popularProfiles.indexOf(user), 1);
+            this.loadMore();
+          }
+        },
         err => {
-      },
-      () => {
-        this.changeDetector.markForCheck();
-      }
-    );
-    
+        },
+        () => {
+          this.changeDetector.markForCheck();
+        }
+      );
+
 
   }
 
-  unsubscribe(user : User) {
+  unsubscribe(user: User) {
     let body = JSON.stringify({
       profileId: user._id
     });
@@ -147,20 +150,20 @@ if (this.popularProfiles.length<6){
       environment.SERVER_URL + pathUtils.UNSUBSCRIBE,
       body,
       AppSettings.OPTIONS)
-      .map((res:Response) => res.json())
+      .map((res: Response) => res.json())
       .subscribe(
         response => {
-        if (response.status == 0) {
-          user.isFollowed = false;
-          user.nbSuivi--;
-        }
-      },
+          if (response.status == 0) {
+            user.isFollowed = false;
+            user.nbSuivi--;
+          }
+        },
         err => {
-      },
-      () => {
-        this.changeDetector.markForCheck();
-      }
-    );
+        },
+        () => {
+          this.changeDetector.markForCheck();
+        }
+      );
   }
 
 
