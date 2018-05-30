805f054e37edb43ce893fa6ae53fc0555df12708
Mezghanni <mezghanni.hassen@gmail.com>
2018-05-30 11:36:32 +0100

    "Revert "Modularization in steps 7_ cleanup the app module"
    This reverts commit 29f9be71bb12b9b299a458ea4808eeef2c5fb820.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,32 +12,28 @@ import { InfiniteScrollModule } from 'ngx-infinite-scroll';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { GoogleRecaptchaDirective } from './directives/googlerecaptcha';
 import { LoginService } from './login/services/loginService';
 import { ResetPasswordService } from './login/services/reset-password.service';
 import { Logout } from './logout/logout';
-import { MainModule } from './main/main.module';
+
 import { DateService } from './main/services/dateService';
 import { EmojiService } from './main/services/emojiService';
 import { GlobalService } from './main/services/globalService';
+import { GoogleRecaptchaDirective } from './directives/googlerecaptcha';
 import { LinkPreview } from './main/services/linkPreview';
 import { LinkView } from './main/services/linkView';
 import { PostService } from './main/services/postService';
 import { RecentRechService } from './main/services/recentRechService';
 import { SeoService } from './main/services/seo-service';
 import { FakeComponent } from './shared/fake.component';
+
 import { httpFactory } from './utils/factories/http.factory';
+import { MainModule } from './main/main.module';
 
 /** Factories */
 @NgModule({
-    imports: [BrowserModule,
-       CommonModule,
-        FormsModule, 
-        InfiniteScrollModule  ,
-      ReactiveFormsModule, 
-      HttpModule, RouterModule,
-       AppRoutingModule,
-       Ng2ImgMaxModule,
+    imports: [BrowserModule, CommonModule, FormsModule, InfiniteScrollModule  ,
+      ReactiveFormsModule, HttpModule, RouterModule, AppRoutingModule,Ng2ImgMaxModule,
       HttpClientModule,
       MainModule,
       TranslateModule.forRoot({
@@ -47,8 +43,7 @@ import { httpFactory } from './utils/factories/http.factory';
           deps: [HttpClient]
         }
       })],       // module dependencies
-    declarations: [AppComponent, 
-       Logout ,
+    declarations: [AppComponent,  Logout ,
          GoogleRecaptchaDirective,
         FakeComponent],   // components and directives
     bootstrap: [AppComponent],     // root component
