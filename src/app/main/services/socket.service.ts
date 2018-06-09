be23c33820f1988c44dc7cc0e91232509d4fa7ee
Ben Mbarek Mohamed Amin <medaminbenmbarek@gmail.com>
2018-06-09 04:14:47 +0200

    Update socket.service.ts #9

--- src/app/main/services/socket.service.ts
+++ src/app/main/services/socket.service.ts
@@ -17,7 +17,7 @@ export class SocketService {
 	* Method to connect the users to socket
 	*/
 	connectSocket(userId: string): void {
-		this.socket = io(this.BASE_URL, { transports: ['websocket'] , query: `userId=${userId}` } );
+		this.socket = io(this.BASE_URL, {  query: `userId=${userId}`, transports: ['websocket']  } );
     }
 
     sendMessage(message){
