c5673439cec267ae21f22a18fba58c6b53a399a5
Ben Mbarek Mohamed Amin <medaminbenmbarek@gmail.com>
2018-06-09 03:08:35 +0200

    Update socket.service.ts try to solve issue #7

--- src/app/main/services/socket.service.ts
+++ src/app/main/services/socket.service.ts
@@ -17,7 +17,7 @@ export class SocketService {
 	* Method to connect the users to socket
 	*/
 	connectSocket(userId: string): void {
-		this.socket = io(this.BASE_URL, { transports: ['websocket'] },{ query: `userId=${userId}` } );
+		this.socket = io(this.BASE_URL, { transports: ['websocket', 'polling'] , query: `userId=${userId}` } );
     }
 
     sendMessage(message){
