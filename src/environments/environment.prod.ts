53bf6cf8addde5bc67e084533625167ac3f9bdfc
touaiti <touaitiahmed@gmail.com>
2018-06-17 20:06:56 +0100

    update environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,15 @@ export const environment = {
   production: true,
 
  SERVER_URL: 'https://speegar.com:3005/',
- IMAGE_BASE_URL: 'https://storage1.speegar.com/images/'
+ IMAGE_BASE_URL: 'https://storage1.speegar.com/images/',
+
+  //Todo : change with speegar account
+   firebase: {
+    apiKey: "AIzaSyAnCqxH5CTNWksJH6j59jIKjxkVJOyEyIk",
+    authDomain: "speegar-6deca.firebaseapp.com",
+    databaseURL: "https://speegar-6deca.firebaseio.com",
+    projectId: "speegar-6deca",
+    storageBucket: "speegar-6deca.appspot.com",
+    messagingSenderId: "861552240215"
+  }
 };
