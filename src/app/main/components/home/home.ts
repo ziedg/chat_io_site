e13aaf897bc9bca9c0fd112e42aad75243ce9940
Abdellaziz <Abdellaziz.Troudi@insat.u-carthage.tn>
2018-06-18 18:13:06 +0200

    Revert "removing console logs"
    This reverts commit a93e48ff403885bf71bb4d4a39d0b81dde5151d5.

--- src/app/main/components/home/home.ts
+++ src/app/main/components/home/home.ts
@@ -1,3 +1,4 @@
+import { SocketService } from './../../services/socket.service';
 import 'rxjs/add/operator/map';
 
 import { ChangeDetectionStrategy, ChangeDetectorRef, Component } from '@angular/core';
@@ -19,6 +20,7 @@ import { LinkPreview } from '../../services/linkPreview';
 import { LinkView } from '../../services/linkView';
 import { NotificationService } from '../../services/notification.service';
 import { PostService } from '../../services/postService';
+
 
 
 declare var jQuery: any;
@@ -72,7 +74,8 @@ export class Home {
 	arabicRegex:RegExp = /[\u0600-\u06FF]/;
   public imageFromLink:boolean = false;
 
-  translationLanguages: Array<String> = ['en', 'fr', 'es'];
+  translationLanguages = [{ name : 'English (US)',value: 'en'},
+                          { name : 'Français (France)',value: 'fr'} ,{ name : '     Español (España)',value: 'es'}];
   selectedLanguage: String;
 
 
@@ -99,9 +102,27 @@ export class Home {
 
     //Notiifcation
     public notificationService: NotificationService,
+    private socketService:SocketService,
 
     private ref:ChangeDetectorRef
   ) {
+
+    ///try socket
+
+   const user= new Promise((resolve,reject)=>{
+  resolve(this.loginService.getUser())
+  
+  }
+    )
+   // connect to socket
+        console.log('connect to socket from main')
+        user.then(user => {
+        this.socketService.connectSocket((user as any)._id);
+
+        })
+    
+
+
     this.isSubscribed = true;
     this.loginService.redirect();
 
@@ -127,13 +148,14 @@ export class Home {
 
   ngOnInit() {
     this.selectedLanguage = localStorage.getItem('userLang');
+    this.socketService.receiveNotifications().subscribe(data => console.log(data))
 
     //Notification Check
         if ('serviceWorker' in navigator && 'PushManager' in window) {
           navigator.serviceWorker.register('assets/sw.js').then(reg => {
           this.registration = reg;
           this.notificationService.init(reg);
-            //console.log('Service Worker and Push is supported');
+            console.log('Service Worker and Push is supported');
           });
       } else {
           console.warn('Push messaging is not supported');
@@ -338,7 +360,7 @@ export class Home {
       return 1;
     }
 		if (text.search(/(\.jpg)|(\.jpeg)|(\.png)|(\.gif)$/i) > 0) {
-			//console.log("image detected");
+			console.log("image detected");
 			jQuery("#preview-image").attr("src", text);
 			jQuery(".file-input-holder").show();
 			jQuery("#preview-image").show();
@@ -666,7 +688,7 @@ getPageFacebookVideo(videoLink): string {
     videoLink.indexOf("m.facebook.com") > 0 || videoLink.indexOf("mobile.facebook.com") > 0 ){
        videoId = this.getIdFacebookVideo(videoLink);
        var videoPage = this.getPageFacebookVideo(videoLink);
-       //console.log("faceboook");
+       console.log("faceboook");
        try {
         jQuery(".youtube-preview").html("");
         jQuery(".facebook-preview").html(
@@ -760,7 +782,7 @@ getPageFacebookVideo(videoLink): string {
               this.resetPublishPicture();
               jQuery(".video-preview").html("");
               //this.form.controls.publicationYoutubeLink.updateValue('');
-              //console.log("hellooooooo");
+              console.log("hellooooooo");
               var r = /:\/\/(.[^/]+)/;
               this.linkDomain= linkURL.match(r)[1] ;
 //              this.link.url = linkURL.substring(0, linkURL.length - 6);
@@ -815,7 +837,7 @@ getPageFacebookVideo(videoLink): string {
     jQuery(".dropdown-menu-translate").hide();
     localStorage.setItem('userLang',language);
     this.translate.setDefaultLang(language);
-    //console.log(localStorage.getItem('userLang')) ;
+    console.log(localStorage.getItem('userLang')) ;
   }
 
   toggleTranslateDropdown() {
--- src/app/main/components/publication/publication.ts
+++ src/app/main/components/publication/publication.ts
@@ -318,7 +318,7 @@ export class Publication {
     this.commentTextareaId = "comment-" + this.publicationBean._id;
     this.changeDetector.markForCheck();
     if (this.publicationBean) {
-      //console.log(this.publicationBean._id);
+      console.log(this.publicationBean._id);
       this.pubLink = urlEncode(environment.SERVER_URL + "main/post/" + this.publicationBean._id);
       this.shareLink = "https://www.facebook.com/sharer/sharer.php?u=" + this.pubLink + "&amp;src=sdkpreparse";
 
@@ -674,7 +674,7 @@ export class Publication {
 
     });
 
-    //console.log(this.publicationBean._id);
+    console.log(this.publicationBean._id);
 
     this.http.post(environment.SERVER_URL + pathUtils.LIKE_PUBLICATION, body, AppSettings.OPTIONS)
       .map((res: Response) => res.json())
@@ -783,8 +783,8 @@ export class Publication {
                   response => { 
                     Array.prototype.push.apply(this.InteractionsLikes, response.message.likes);
                     Array.prototype.push.apply(this.InteractionsDislikes, response.message.dislikes);
-                    //console.log(this.InteractionsLikes);
-                    //console.log(this.InteractionsDislikes);
+                    console.log(this.InteractionsLikes);
+                    console.log(this.InteractionsDislikes);
                 },
                 err => {
                   console.error('Cannot get interactions');
