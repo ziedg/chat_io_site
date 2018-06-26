fec1e230a3658a8014fc44a165c5970df4affc02
DESKTOP-JLS45L2\BahaEddine <baha@peaksource.tn>
2018-06-26 01:31:11 +0100

    Suggestions Design

--- src/app/messaging/suggestions/suggestions.component.ts
+++ src/app/messaging/suggestions/suggestions.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit, Input } from '@angular/core';
   styleUrls: ['./suggestions.component.css']
 })
 export class SuggestionsComponent implements OnInit {
-  @Input() users;
+  @Input() user;
   constructor() { }
 
   ngOnInit() {
