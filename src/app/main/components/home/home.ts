7cbf9dd214394d8a7cde0f1a26650bc03d1f7840
walimkr <islemmseddi@gmail.com>
2018-06-28 00:28:49 +0100

    fix text pub

--- src/app/main/components/home/home.ts
+++ src/app/main/components/home/home.ts
@@ -730,7 +730,7 @@ export class Home {
       for(let i=0;i<line_parts.length;i++){
         nb=nb+(line_parts[i].length/39);
         if(line_parts[i].length%39!=0) nb++;}
-      if(nb>14){
+      if(nb>8){
         this.getbg0();
         this.bgvalid=false;
       }
