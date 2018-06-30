89d3b0cc37211b4685988a2affec20242b48e07d
samet <mohamedamin.samet@yandex.com>
2018-07-01 00:53:47 +0100

    available picture

--- src/app/main/components/profile/pipes/AvailablePicture.pipe.ts
+++ src/app/main/components/profile/pipes/AvailablePicture.pipe.ts
@@ -1,5 +1,6 @@
 import {Pipe, PipeTransform} from "@angular/core";
 import {User} from "../../../../beans/user";
+import { PublicationBean } from "../../../../beans/publication-bean"
 
 
 @Pipe({
@@ -7,7 +8,7 @@ import {User} from "../../../../beans/user";
 })
 export class AvailablePicture implements PipeTransform {
   alternativeProfilePicture: string = 'http://www.personalbrandingblog.com/wp-content/uploads/2017/08/blank-profile-picture-973460_640-300x300.png';
-  transform(profile) {
+  transform(profile: User|PublicationBean) {
     let profilePicture:string = profile.profilePictureMin ? profile.profilePictureMin : profile.profilePicture;
     profilePicture = profilePicture.includes('avatars') ? this.alternativeProfilePicture : profilePicture;
     return profilePicture;
