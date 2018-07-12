0d454851b2d282e083fd29e331bb547385898a54
samet <mohamedamin.samet@yandex.com>
2018-07-12 16:56:52 +0100

    interactions modal component / subscribe

--- src/app/main/components/publication/interactions-modal/interactions-modal.ts
+++ src/app/main/components/publication/interactions-modal/interactions-modal.ts
@@ -4,6 +4,8 @@ import { MinifiedUser } from "../../../../beans/Minified-user";
 import { CommentBean } from "../../../../beans/comment-bean";
 import { AppSettings } from "../../../../shared/conf/app-settings";
 import * as pathUtils from "../../../../utils/path.utils";
+import { User } from "../../../../beans/user";
+import { LoginService } from "../../../../login/services/loginService";
 
 
 
@@ -22,33 +24,34 @@ export class InteractiosModal implements OnInit, OnDestroy {
     public modalInteractions = false;
     interactionsLoaded:boolean=false;
     imageBaseUrl = environment.IMAGE_BASE_URL;
+    profileId:string;
+
+    isTogglingSubscribe: boolean = true;
 
     @Output("closeModal") eventCloseModal = new EventEmitter()
 
-
     @Input("pubId") pubId: string;
+    @Input("user") user: User;
     @Input("nbLikes") nbLikes: number;
     @Input("nbDislikes") nbDislikes: number;
 
     constructor(private http: Http,
-                private changeDetector: ChangeDetectorRef,) {
+                private changeDetector: ChangeDetectorRef,
+                public loginService: LoginService) {
+      this.profileId = this.loginService.user._id;
     }
 
     ngOnInit() {
-        console.log("opent interaction modal", this.pubId);
         document.body.style.overflow = "hidden";
         this.getInteractions();
     }
 
     getInteractions() {
-        console.log("getInteractions");
-        var url: string =
-          environment.SERVER_URL + pathUtils.GET_SOCIAL_INTERACTIONS;
+        var url: string =environment.SERVER_URL + pathUtils.GET_SOCIAL_INTERACTIONS;
     
         let body = JSON.stringify({
           publId: this.pubId,
-          page: this.interactionsPage
-        });
+          page: this.interactionsPage});
     
         this.http
           .post(url, body, AppSettings.OPTIONS)
@@ -58,8 +61,6 @@ export class InteractiosModal implements OnInit, OnDestroy {
               this.interactionsLoaded=true;
               this.InteractionsLikes = response.message.likes.slice();
               this.InteractionsDislikes = response.message.dislikes.slice();
-              console.log(this.InteractionsLikes);
-              console.log(this.InteractionsDislikes);
             },
             err => { 
               this.interactionsLoaded=true;
@@ -92,10 +93,82 @@ export class InteractiosModal implements OnInit, OnDestroy {
         currentelem.style.display = "block";
         currentelem.className += " active";
       }
+    
+
+      toggleSubscribe(user) {
+        this.isTogglingSubscribe = false;
+        if (user.isSubscribed === "true") {
+          this.unsubscribeUser(user.userId);
+          
+          let like = this.InteractionsLikes.filter(x => x.userId === user.userId)[0];
+          if (like != undefined) { like.isSubscribed = "false"; }
+          let dislike = this.InteractionsDislikes.filter(x => x.userId === user.userId)[0];
+          if (dislike != undefined) { dislike.isSubscribed = "false";}
+        }
+        else {
+          if (user.isSubscribed === "false") {
+            this.subscribeUser(user.userId);
+            let like = this.InteractionsLikes.filter(x => x.userId === user.userId)[0];
+            if (like != undefined) { like.isSubscribed = "true";}
+            let dislike = this.InteractionsDislikes.filter(x => x.userId === user.userId)[0];
+            if (dislike != undefined) { dislike.isSubscribed = "true";}
+          }
+        }
+      }
+
+    
+      subscribeUser(userId) {
+        let body = JSON.stringify({profileId: userId});
+    
+        this.http
+          .post(
+            environment.SERVER_URL + pathUtils.SUBSCRIBE,
+            body,
+            AppSettings.OPTIONS
+          )
+          .map((res: Response) => res.json())
+          .subscribe(
+            response => {
+              if (response.status == 0) {}
+            },
+            err => { },
+            () => {
+              this.changeDetector.markForCheck();
+              this.isTogglingSubscribe = true;
+            }
+          );
+      }
+    
+      unsubscribeUser(userId) {
+        let body = JSON.stringify({profileId: userId});
+    
+        this.http
+          .post(
+            environment.SERVER_URL + pathUtils.UNSUBSCRIBE,
+            body,
+            AppSettings.OPTIONS)
+          .map((res: Response) => res.json())
+          .subscribe(
+            response => {
+              if (response.status == 0) {
+              }
+            },
+            err => { },
+            () => {
+              this.changeDetector.markForCheck();
+              this.isTogglingSubscribe = true;
+            }
+          );
+      }
+    
 
 
     closeModalInteractions() {
-        if(this.interactionsLoaded) this.eventCloseModal.emit();
+        if(this.interactionsLoaded && this.isTogglingSubscribe) this.eventCloseModal.emit();
+    }
+
+    clickOutside(){
+        setTimeout(()=> this.closeModalInteractions(), 10);
     }
 
     ngOnDestroy() {
